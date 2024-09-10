6de5c9c6f20b7ed7216fb9378fd714f4d0870c92
Evan Perry <eperry2688@gmail.com>
2024-09-10 16:39:01 -0600

    11.9 Simpple end-to-end tests, both tests

--- e2e-tests/playwrightTest.test.js
+++ e2e-tests/playwrightTest.test.js
@@ -1,9 +1,15 @@
+// eslint-disable-next-line no-unused-vars
 const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
     await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+  test('Navigate to a particular pokemon (ivysaur)', async ({ page }) => {
+    await page.goto('/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 })
